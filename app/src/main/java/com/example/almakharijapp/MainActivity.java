90738fb4df6c36696489c5792fb76c6215f8414a
HumaaMustafa <bitf18a520@pucit.edu.pk>
2021-11-09 21:23:54 +0500

    4th

--- app/src/main/java/com/example/almakharijapp/MainActivity.java
+++ app/src/main/java/com/example/almakharijapp/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.almakharijapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button , button2;
+    private Button button , button2 , repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +36,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        repo = (Button) findViewById(R.id.button3);
+        repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                gotoURL("https://github.com/HumaaMustafa/AlMakharij");
+
+
+            }
+        });
+
+
+
     }
+
+    private void gotoURL(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri) );
+    }
+
     public void openActivity2()
     {
         Intent intent = new Intent(this , MainActivity2.class);
