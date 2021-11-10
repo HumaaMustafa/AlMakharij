73311a29c586bc8643212473beba0b9745964b16
HumaaMustafa <bitf18a520@pucit.edu.pk>
2021-11-11 01:09:20 +0500

    7th

--- app/src/main/java/com/example/almakharijapp/MainActivity2.java
+++ app/src/main/java/com/example/almakharijapp/MainActivity2.java
@@ -2,14 +2,34 @@ package com.example.almakharijapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private List<QuizQuestions>questionsList;
+    private TextView Ques , Score;
+    private RadioGroup radioG;
+    private RadioButton r1 , r2, r3;
+    private Button next;
+
+    int totalQues , counter=0 , scor ;
+
+    ColorStateList color;
+    boolean ans;
+
+    private List<QuizQuestions> questionsList;
+
+    private QuizQuestions currentQ;
 
 
     @Override
@@ -18,11 +38,112 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         questionsList = new ArrayList<>();
+        Ques = findViewById(R.id.textView3);
+        Score = findViewById(R.id.textView4);
+
+        radioG = findViewById(R.id.radioGroup);
+        r1 = findViewById(R.id.op1);
+        r2 = findViewById(R.id.op2);
+        r3 = findViewById(R.id.op3);
+
+        next = findViewById(R.id.next3);
+
+        color = r1.getTextColors();
+
 
         addQuestions();
+        totalQues = questionsList.size();
+        showNextQuestion();
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ans == false) {
+                    if (r1.isChecked() || r2.isChecked() || r3.isChecked()) {
+                        checkAns();
+
+                    } else {
+                        Toast.makeText(MainActivity2.this, "Please Select an option", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    showNextQuestion();
+                }
+            }
+        });
+    }
+
+    private void checkAns() {
+        ans = true;
+        RadioButton rSelected= findViewById(radioG.getCheckedRadioButtonId());
+        int ansNO = radioG.indexOfChild(rSelected)+1;
+        if(ansNO==currentQ.getAnsCorr()){
+            scor++;
+            Score.setText("Score: "+scor);
+        }
+        r1.setTextColor(Color.RED);
+        r2.setTextColor(Color.RED);
+        r3.setTextColor(Color.RED);
+        switch(currentQ.getAnsCorr()){
+            case 1:
+                r1.setTextColor(Color.GREEN);
+                break;
+
+            case 2:
+                r2.setTextColor(Color.GREEN);
+                break;
+
+            case 3:
+                r3 .setTextColor(Color.GREEN);
+                break;
+
+        }
+        if(counter<totalQues){
+            next.setText("Next");
+
+        }else{
+            next.setText("Finish");
+        }
+    }
+
+
+    private void showNextQuestion() {
+         radioG.clearCheck();
+         r1.setTextColor(color);
+         r2.setTextColor(color);
+         r3.setTextColor(color);
+        if (counter< totalQues){
+            currentQ = questionsList.get(counter);
+            Ques.setText(currentQ.getQuestion());
+            r1.setText(currentQ.getOpt1());
+            r2.setText(currentQ.getOpt2());
+            r3.setText(currentQ.getOpt3());
+            counter++;
+            next.setText("Submit");
+            ans = false;
+        }
+        else{
+            finish();
+        }
     }
 
     private void addQuestions() {
-        questionsList.add(new QuizQuestions())
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+        questionsList.add(new QuizQuestions("", "", "", "", 1));
+
+
     }
 }
--- app/src/main/java/com/example/almakharijapp/QuizQuestions.java
+++ app/src/main/java/com/example/almakharijapp/QuizQuestions.java
@@ -2,14 +2,15 @@ package com.example.almakharijapp;
 
 public class QuizQuestions {
     private  String question , opt1 , opt2 , opt3;
-    private  int score;
+    private  int ansCorr;
 
-    public QuizQuestions(String question, String opt1, String opt2, String opt3, int score) {
+
+    public QuizQuestions(String question, String opt1, String opt2, String opt3, int ansCorr) {
         this.question = question;
         this.opt1 = opt1;
         this.opt2 = opt2;
         this.opt3 = opt3;
-        this.score = score;
+        this.ansCorr = ansCorr;
     }
 
     public String getOpt3() {
@@ -44,11 +45,12 @@ public class QuizQuestions {
         this.question = question;
     }
 
-    public int getScore() {
-        return score;
+    public int getAnsCorr() {
+        return ansCorr;
     }
 
-    public void setScore(int score) {
-        this.score = score;
+    public void setAnsCorr(int score) {
+        this.ansCorr = ansCorr;
     }
+
 }
